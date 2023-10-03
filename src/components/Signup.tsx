c9f3be5af654682049b500dec950aa364ecbb30f
mazen <mazenmohamed181@gmail.com>
2023-10-03 09:28:12 +0300

    refact(Signup): Using react-hook-form to manage form state

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -6,32 +6,26 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
-import { FormEvent, useRef } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Signup = () => {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: FormEvent) => {
-    const user = {
-      email: emailRef.current?.value,
-      password: passwordRef.current?.value,
-    };
-    event.preventDefault();
-    if (emailRef.current && passwordRef.current) {
-      console.log(user);
-    }
+  const onSubmit = (data: FieldValues) => {
+    console.log("Submiting", data);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl mb={5}>
         <FormLabel>Email</FormLabel>
-        <Input ref={emailRef} id="email" type="email" />
+        <Input {...register("email")} id="email" type="email" />
         <FormHelperText>We'll never share your email.</FormHelperText>
       </FormControl>
+
       <FormControl mb={5}>
         <FormLabel>Password</FormLabel>
-        <Input ref={passwordRef} id="password" type="password" />
+        <Input {...register("password")} id="password" type="password" />
         <FormHelperText>Your password is encrypted.</FormHelperText>
       </FormControl>
 
