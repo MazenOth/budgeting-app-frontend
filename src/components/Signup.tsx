5fcef656838a04ef7e9d1a18f7466c1e73785599
mazen <mazenmohamed181@gmail.com>
2023-10-03 11:21:05 +0300

    add(Signup): Schema based validation with Zod

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -7,9 +7,22 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().min(5).email(),
+  password: z.string().min(8),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Signup = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     console.log("Submiting", data);
@@ -20,12 +33,14 @@ const Signup = () => {
       <FormControl mb={5}>
         <FormLabel>Email</FormLabel>
         <Input {...register("email")} id="email" type="email" />
+        {errors.email && <p color="tomato">{errors.email.message}</p>}
         <FormHelperText>We'll never share your email.</FormHelperText>
       </FormControl>
 
       <FormControl mb={5}>
         <FormLabel>Password</FormLabel>
         <Input {...register("password")} id="password" type="password" />
+        {errors.password && <p color="tomato">{errors.password.message}</p>}
         <FormHelperText>Your password is encrypted.</FormHelperText>
       </FormControl>
 
