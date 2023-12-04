f105177ce9650c104879a112d3be72e7ea78ddfe
mazen <mazenmohamed181@gmail.com>
2023-12-04 20:01:49 +0200

    add(App): Fetching users dummy data from jsonplaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
 import Signup from "./components/Signup";
+import axios from "axios";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from "@chakra-ui/react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  return <Signup></Signup>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+  return (
+    <>
+      <UnorderedList mb={3}>
+        {users.map((user) => (
+          <ListItem key={user.id}>{user.name}</ListItem>
+        ))}
+      </UnorderedList>
+      <Signup></Signup>
+    </>
+  );
 }
 
 export default App;
