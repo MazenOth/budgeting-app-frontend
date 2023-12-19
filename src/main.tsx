6cf3cba2c5b55f1878010a4ce3d46a35d964ab3c
Mazen <mazenothman.100@gmail.com>
2023-12-19 09:52:54 +0200

    add(theme, main): Dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/routes";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
