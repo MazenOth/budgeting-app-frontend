94d1864dd5560df202dfbf93a24a00a41bcd758d
Mazen <mazenothman.100@gmail.com>
2023-12-19 10:27:08 +0200

    add(ColorModeSwitch, NavBar): Color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import { HStack, Text } from "@chakra-ui/react";
 import { FaWallet } from "react-icons/fa";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack ml={"3"}>
-      <FaWallet size={"30px"} />
-      <Text>Choose Wallet</Text>
+    <HStack ml={"3"} justifyContent={"space-between"} mr={"3"}>
+      <HStack>
+        <FaWallet size={"30px"} />
+        <Text>Choose Wallet</Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
